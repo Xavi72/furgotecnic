50dc8b646e1ad1c9fd3ebc1d4a9beabf3b45f2d9
Xavier <joubertxavier@gmail.com>
2026-03-18 07:24:03 +0100

    add homepage and datas

--- components/views/homepage.tsx
+++ components/views/homepage.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { getDictionary } from "@/app/[lang]/dictionaries";
 import Icon from "@/public/assets/icons/globe.svg";
 
@@ -11,11 +12,69 @@ export default async function HomePage({
   const dict: any = await getDictionary(lang);
 
   return (
-    <div className="flex flex-col min-h-screen items-center justify-center">
-      {dict?.homepage.title}
-
-      <h1 className="text-4xl font-bold">Furgotecnic</h1>
-      <Icon className="w-12 h-12 text-black" />
+    <div className="flex flex-col min-h-screen items-center ">
+      <div
+        id="hero"
+        className="flex flex-col w-full w-[1280px] h-auto mt-[170px]"
+      >
+        <div className="flex w-full h-auto min-h-[400px] bg-black/5 justify-center items-center relative">
+          <img
+            src="/assets/images/interior_wood_large.webp"
+            alt="Hero image Furgotecnic"
+            className="flex w-full h-full min-h-[400px] object-cover object-top"
+          />
+          <div className="absolute bottom-0 left-0 right-0 flex flex-col w-full h-full min-h-[200px] justify-center items-center">
+            <h1 className="text-2xl md:text-4xl font-bold text-white">
+              Soluciones eficientes para
+            </h1>
+            <h1 className="text-2xl md:text-4xl font-bold text-white text-center">
+              vehículos de profesionales
+            </h1>
+          </div>
+        </div>
+      </div>
+      <div
+        id="preparation"
+        className="grid grid-cols-1 md:grid-cols-2 flex flex-col w-full max-w-[1280px] h-auto p-4 gap-4"
+      >
+        {dict.homepage.preparation.items.map(
+          (item: { title: string; content?: string[] }, index: number) => (
+            <div
+              key={index}
+              className="flex flex-row w-full h-auto justify-center px-4 py-6 bg-tertiary rounded-sm gap-2"
+            >
+              <div
+                key={index}
+                className="flex flex-col w-1/2 h-full justify-center bg-tertiary rounded-sm gap-3"
+              >
+                <h2 className="text-base md:text-lg font-normal text-white ">
+                  {item.title}
+                </h2>
+                <div className="flex flex-row w-auto h-auto ml-2 gap-2 items-center active:scale-95 transition-transform  cursor-pointer">
+                  <p className="text-sm text-white  underline underline-offset-4 decoration-primary">
+                    Ver más
+                  </p>
+                  <p className="text-base text-primary">{`-->`}</p>
+                </div>
+              </div>
+              <div className="flex flex-col w-1/2 min-w-[200px] h-full justify-center bg-tertiary rounded-sm gap-2">
+                {item?.content?.map((content: string, index: number) => (
+                  <div
+                    key={index}
+                    className="flex flex-row w-auto h-auto gap-2 items-center"
+                  >
+                    <div className="w-2 h-2 bg-primary rounded-full" />
+                    <p className="text-sm text-white">{content}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ),
+        )}
+      </div>
+      <div id="services" className="flex flex-col w-full w-[1280px] h-auto">
+        <div className="w-full h-[1000px] "></div>
+      </div>
     </div>
   );
 }
---
+++ utils/webdatas.tsx
@@ -0,0 +1,127 @@
+export const navigationLink = [
+  {
+    name: "Preparación",
+    slug: "preparacion",
+    href: "/preparacion",
+    subLinks: [
+      {
+        name: "Seguridad",
+        slug: "seguridad",
+        href: "/preparacion/seguridad",
+      },
+      {
+        name: "Protección interior",
+        slug: "proteccion-interior",
+        href: "/preparacion/proteccion-interior",
+      },
+      {
+        name: "Sujeción de carga",
+        slug: "sujecion-carga",
+        href: "/preparacion/sujecion-carga",
+      },
+      {
+        name: "Equipamiento modular",
+        slug: "equipamiento-modular",
+        href: "/preparacion/equipamiento-modular",
+      },
+      {
+        name: "Rotulación",
+        slug: "rotulacion",
+        href: "/preparacion/rotulacion",
+      },
+    ],
+  },
+  {
+    name: "Mantenimiento",
+    slug: "mantenimiento",
+    href: "/mantenimiento",
+    subLinks: [
+      {
+        name: "Mecánica general",
+        slug: "mecanica-general",
+        href: "/mantenimiento/mecanica-general",
+      },
+      {
+        name: "Neumáticos",
+        slug: "neumaticos",
+        href: "/mantenimiento/neumaticos",
+      },
+      {
+        name: "Reacondicionamiento",
+        slug: "reacondicionamiento",
+        href: "/mantenimiento/reacondicionamiento",
+      },
+      {
+        name: "Homologación & ITV",
+        slug: "homologacion",
+        href: "/mantenimiento/homologacion",
+      },
+    ],
+  },
+  {
+    name: "Transformación",
+    slug: "transformacion",
+    href: "/transformacion",
+    subLinks: [
+      {
+        name: "Camper",
+        slug: "camper",
+        href: "/transformacion/camper",
+      },
+      {
+        name: "Food Truck",
+        slug: "food-truck",
+        href: "/transformacion/food-truck",
+      },
+      {
+        name: "Motorsport",
+        slug: "motorsport",
+        href: "/transformacion/motorsport",
+      },
+      {
+        name: "Especializada",
+        slug: "especializada",
+        href: "/transformacion/especializada",
+      },
+      {
+        name: "Reserva tu slot",
+        slug: "reserva-tu-slot",
+        href: "/transformacion/reserva-tu-slot",
+      },
+    ],
+  },
+  {
+    name: "Accesorios",
+    slug: "accesorios",
+    href: "/accesorios",
+    subLinks: [
+      {
+        name: "Para furgonetas comerciales",
+        slug: "para-furgonetas",
+        href: "/accesorios/para-furgonetas",
+      },
+      {
+        name: "Para Campers",
+        slug: "para-campers",
+        href: "/accesorios/para-campers",
+      },
+    ],
+  },
+  {
+    name: "Vehículos en venta",
+    slug: "vehiculos-en-venta",
+    href: "/vehiculos-en-venta",
+    subLinks: [
+      {
+        name: "Furgonetas disponibles",
+        slug: "furgonetas-disponibles",
+        href: "/vehiculos-disponibles/furgonetas-disponibles",
+      },
+      {
+        name: "Vehículos equipados",
+        slug: "vehiculos-equipados",
+        href: "/vehiculos-disponibles/vehiculos-equipados",
+      },
+    ],
+  },
+];
